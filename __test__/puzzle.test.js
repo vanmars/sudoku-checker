1e0427427fa9e79d21537c7cb36fbaaff248f218
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-01 16:31:06 -0700

    Pass test: should create rows in puzzle

--- __test__/puzzle.test.js
+++ __test__/puzzle.test.js
@@ -5,12 +5,79 @@ describe("Puzzle", () => {
   test("should correctly create a puzzle object with rows and columns", () => {
     const puzzle = new Puzzle();
     expect(puzzle.rows).toEqual([]);
-    expect(puzzle.columns).toEqual([]);
+    expect(puzzle.columns).toEqual([[], [], [], [], [], [], [], [], []]);
   });
   test("should correctly create a row array in the puzzle", () => {
-    const rowArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const puzzle = new Puzzle();
-    puzzle.addRow(rowArr);
-    expect(puzzle.rows).toEqual([[1, 2, 3, 4, 5, 6, 7, 8, 9]])
+    puzzle.addRow(rowArr1);
+    puzzle.addRow(rowArr2);
+    expect(puzzle.rows).toEqual([[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]]);
+  });
+  test("should correctly create a column array from row arrays", () => {
+    const rowArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const puzzle = new Puzzle();
+    puzzle.addRow(rowArr1);
+    puzzle.addRow(rowArr2);
+    puzzle.addRow(rowArr3);
+    puzzle.addRow(rowArr4);
+    puzzle.addRow(rowArr5);
+    puzzle.addRow(rowArr6);
+    puzzle.addRow(rowArr7);
+    puzzle.addRow(rowArr8);
+    puzzle.addRow(rowArr9);
+    // console.log(puzzle.rows[0]);
+    // console.log(puzzle.rows[0][1]);
+    // console.log(puzzle.rows[1]);
+    // console.log(puzzle.rows[1][4]);
+    puzzle.addColumn();
+    console.log(puzzle.columns);
+    expect(puzzle.columns).toEqual([[1, 1, 1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2, 2, 2, 2], [3, 3, 3, 3, 3, 3, 3, 3, 3], [4, 4, 4, 4, 4, 4, 4, 4, 4], [5, 5, 5, 5, 5, 5, 5, 5, 5], [6, 6, 6, 6, 6, 6, 6, 6, 6], [7, 7, 7, 7, 7, 7, 7, 7, 7], [8, 8, 8, 8, 8, 8, 8, 8, 8], [9, 9, 9, 9, 9, 9, 9, 9, 9]]);
+  });
+  test('should check that all rows have nine elements with each number 1-9', () => {
+    const rowArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const rowArr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const puzzle = new Puzzle();
+    puzzle.addRow(rowArr1);
+    puzzle.addRow(rowArr2);
+    puzzle.addRow(rowArr3);
+    puzzle.addRow(rowArr4);
+    puzzle.addRow(rowArr5);
+    puzzle.addRow(rowArr6);
+    puzzle.addRow(rowArr7);
+    puzzle.addRow(rowArr8);
+    puzzle.addRow(rowArr9);
+    puzzle.addColumn();
+    console.log(puzzle.checkPuzzle);
+    expect(puzzle.checkPuzzle).toEqual("Row 0 passes the test.");
   })
 });
+
+
+
+
+// Puzzle.prototype.checkPuzzle = function () {
+//   for (let i=0; i<this.rows.length; i++) {
+//     if (this.rows[i].length === 9 && this.rows[i].includes(1, 2, 3, 4, 5, 6, 7, 8, 9)) {
+//       return ("Row " + [i] + " passes the test.");
+//     } else {
+//       return ("Test failed - Try harder");
+//     };  
+//   };
+// };
--- src/puzzle.js
+++ src/puzzle.js
@@ -1,28 +1,46 @@
 // Create a constructor for a puzzle with row and column properties
 export default function Puzzle (){
   this.rows = [];
-  this.columns = [];
-}
+  this.columns = [[], [], [], [], [], [], [], [], []];
+};
 
 Puzzle.prototype.addRow = function ([num1, num2, num3, num4, num5, num6, num7, num8, num9]){
   this.rows.push([num1, num2, num3, num4, num5, num6, num7, num8, num9]);
-}
+};
 
 Puzzle.prototype.addColumn = function () {
     for (let i=0; i<this.rows.length; i++) {
-      for(numIndex=0; numIndex<9; numIndex++){
+      for(let numIndex=0; numIndex<9; numIndex++){
+        // console.log(this.rows[numIndex][i]);
         this.columns[i].push(this.rows[numIndex][i]);
       };
     };
+   
+Puzzle.prototype.checkPuzzle = function () {
+  let result;
+  for (let i=0; i<this.rows.length; i++) {
+    if (this.rows[i].length === 9 && this.rows[i].includes(1, 2, 3, 4, 5, 6, 7, 8, 9)) {
+      result = ("Row " + [i] + " passes the test.");
+    } else {
+      result = ("Test failed - Try harder");
+    };  
+  };
+  return result;
 };
+
+
+
+//loop through rows check:
+// a) rows.length = 9
+// b) 1-9 are present
+
 
 // Method: Check Rows
 
 
 // Method: Check Columns
 
-
-// Scratch Work
+ // Scratch Work
   // 1, 2, 3, 4, 5
   // 2, 3, 4, 5, 1
   // 3, 4, 5, 1, 2
@@ -35,3 +53,6 @@ Puzzle.prototype.addColumn = function () {
   
 //  col[0] row[0][0]
 //  col[0] row[0][1]
+};
+
+
